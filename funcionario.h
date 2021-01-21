c2c2b1e0f442405130c9ce658e26926aa24c757d
Pedro <fernandes.slb.02.pf@gmail.com>
2021-01-21 14:01:05 +0000

    //

--- funcionario.h
+++ funcionario.h
@@ -20,7 +20,10 @@ typedef struct{
 }Funcionario;
 
 /*functions/procedure declaration*/
-void criarfuncionario(Funcionario *funcionario, int n);
+void criarFuncionario(Funcionario *funcionario, int n);
+void quantFuncionario(Funcionario *funcionario, int n);
+void mediaIdadesFuncionario(Funcionario *funcionario, int n);
+void listarMedicosVenci(Funcionario *funcionario, int n);
 
 
 #endif
